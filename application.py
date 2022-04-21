f0a1a94de50b9581bbb2dd1d83d0a7c0ce04d1f9
Dave Stephenson <dave.stephenson@blueyonder.co.uk>
2022-04-21 16:45:15 +0100

    added full parameter & return

--- application.py
+++ application.py
@@ -12,16 +12,49 @@ api = Api(application)
 
 
 class AddressHygieneApi(Resource):
-   def get(self):
-       result = {'employees': [{'id':1, 'name':'Balram'},{'id':2, 'name':'Tom'}]}
-       return result
 
-api.add_resource(AddressHygieneApi, '/api/ahs') # Route_1
+    def post(self):
+
+        # This defines the expected POST variables (including a json payload)
+        parser = reqparse.RequestParser()
+        parser.add_argument('ADDRESS1', type=str)
+        parser.add_argument('ADDRESS2', type=str)
+        parser.add_argument('ADDRESS3', type=str)
+        parser.add_argument('ADDRESS4', type=str)
+        parser.add_argument('ADDRESS5', type=str)
+        parser.add_argument('ADDRESS6', type=str)
+        parser.add_argument('ADDRESS7', type=str)
+        parser.add_argument('ADDRESS8', type=str)
+        parser.add_argument('ADDRESS9', type=str)
+        parser.add_argument('POSTCODE', type=str)
+       args = parser.parse_args()
+
+        # This sets the return values
+        response['PAF_POBOX'] = ''
+        response['PAF_ABODE'] = ''
+        response['PAF_HOUSENAME'] = ''
+        response['PAF_HOUSENUM'] = args['ADDRESS1']
+        response['PAF_STREET1'] = args['ADDRESS2']
+        response['PAF_STREET1TYPE'] = ''
+        response['PAF_STREET2'] = args['ADDRESS3']
+        response['PAF_SUBLOCALITY'] = ''
+        response['PAF_LOCALITY'] = ''
+        response['PAF_TOWN'] = args['ADDRESS4']
+        response['PAF_POSTCODE'] = args['POSTCODE']
+        response['PAF_DPS'] = ''
+        response['PAFEXCEPTIONMESSAGE'] = 'OK'
+        response['PAFENGINEDATE'] = 'Jan 2022'
+        response['PAFSTATUS'] = 'OK'
+
+        return jsonify(response)
+
+
+api.add_resource(AddressHygieneApi, '/api/ahs', methods = ['POST']) # Route_1
 
 
 @application.route("/")
 def index():
-      return render_template('index.html')
+      return render_template('index.html', status='OK')
 
 
 if (__name__ == '__main__'):
